9124513f01740ed41c1b036f6b34c0085948a560
NamelessExplorer <91269749+NamelessExplorer@users.noreply.github.com>
2024-09-13 18:03:37 +0530

    Removed child process module

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const allergyRoutes = require('./routes/allergyRoutes');
-const { spawn } = require('child_process');
 
 
 const app = express();
@@ -26,28 +25,7 @@ app.post('/chatapi', (req, res) => {
   const message = req.body.message;
 
   // Get the Hugging Face API key from the environment variables
-  const huggingFaceApiKey = process.env.HUGGING_FACE_API_KEY;
-
-  // Spawn a Python process and pass the Hugging Face API key as well
-  const pythonProcess = spawn('python3', ['./pythonfile/chatCompletion.py', JSON.stringify({ message })]);
-
-  // Collect the output from the Python script
-  let output = '';
-  pythonProcess.stdout.on('data', (data) => {
-      output += data.toString();
-  });
-
-  // Send the response when the process is done
-  pythonProcess.stdout.on('end', () => {
-      res.send({ response: output });
-  });
-
-  // Handle any errors from the Python process
-  pythonProcess.stderr.on('data', (data) => {
-      console.error('Error:', data.toString());
-      res.status(500).send('Internal Server Error');
-  });
-});
+  const huggingFaceApiKey = process.env.HUGGING_FACE_API_KE
 
 
 app.listen(PORT, () => {
